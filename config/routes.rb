74f920aaff18e13812580583ecd01e3b0b06eac8
Evelin <evelingarcia86@gmail.com>
2015-11-12 14:02:39 -0300

    Remove overrides

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Spree::Core::Engine.add_routes do
-  #devise_for :spree_user,
-  #           class_name: Spree.user_class,
-  #           only: [:omniauth_callbacks],
-  #           controllers: { omniauth_callbacks: 'spree/omniauth_callbacks' },
-  #           path: Spree::SocialConfig[:path_prefix]
-
-  devise_scope :spree_user do
-    match "/users/auth/:action/callback",
-      constraints: { action: /google|facebook|amazon/ },
-      to: "omniauth_callbacks",
-      as: :spree_user_omniauth_callback_backup,
-      via: [:get, :post]
-  end
+  devise_for :spree_user,
+             class_name: Spree.user_class,
+             only: [:omniauth_callbacks],
+             controllers: { omniauth_callbacks: 'spree/omniauth_callbacks' },
+             path: Spree::SocialConfig[:path_prefix]
 
   resources :user_authentications
 
